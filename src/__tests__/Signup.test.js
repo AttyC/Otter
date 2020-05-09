2cf41cda3a9a39065f196da9c228d4424e456e2c
AttyC <antoniajcronin@gmail.com>
2020-05-09 13:21:27 +0100

    Add Tests for Signup

---
+++ src/__tests__/Signup.test.js
@@ -0,0 +1,49 @@
+import validate from '../components/LoginFormValidationRules';
+
+describe('Validate Signup form', () => {
+
+  describe('areAllValuesValid', () => {
+    it('should return an empty object if the values are valid', () => {
+      const mockValues = {
+        name: 'AcmeAnvils',
+        email: 'wile.coyote@monumentvalley.com',
+        confirmEmail: 'wile.coyote@monumentvalley.com',
+      };
+      const testValue = validate(mockValues)
+
+      expect(testValue).toEqual({});
+    });
+
+    it('should return errors.name if the name is too short', () => {
+      const mockValues = {
+        name: 'A',
+        email: 'wile.coyote@monumentvalley.com',
+        confirmEmail: 'wile.coyote@monumentvalley.com',
+      }
+      const testValue = validate(mockValues)
+      expect(testValue).toEqual({name: "Name must be 3 or more characters"})
+
+    });
+    it('should return errors.email if the email is not formatted correctly', () => {
+      const mockValues = {
+        name: 'AcmeAnvils',
+        email: 'wile.coyote@monumentvalley',
+        confirmEmail: 'wile.coyote@monumentvalley',
+      }
+      const testValue = validate(mockValues)
+      expect(testValue).toEqual({email: "Email address is invalid", confirmEmail: "Email address is invalid" })
+
+    });
+
+    it('should return errors.confirmEmail if the emails do not match', () => {
+      const mockValues = {
+        name: 'AcmeAnvils',
+        email: 'wile.coyote@monumentvalley.com',
+        confirmEmail: 'road.runner@eatmydust.com',
+      }
+      const testValue = validate(mockValues)
+      expect(testValue).toEqual({confirmEmail: "Email addresses must match"})
+
+    });
+  });
+});
