d7cd3300edb5d0ace8fb9c3da9b0cfcb50dc4337
AttyC <antoniajcronin@gmail.com>
2020-07-01 14:34:27 +0100

    add build folder

---
+++ build/precache-manifest.b15051af0ce61ae06311652fbf1c5ead.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcde37253656615ff3f8a2dcf00abf44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d289ff5f7d932c69f53",
+    "url": "/static/css/main.e9278b5e.chunk.css"
+  },
+  {
+    "revision": "4f6d297f4bc5860efc4e",
+    "url": "/static/js/2.196e84bc.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/static/js/2.196e84bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d289ff5f7d932c69f53",
+    "url": "/static/js/main.ba864cbb.chunk.js"
+  },
+  {
+    "revision": "6c58d8d0828b36c1c49b",
+    "url": "/static/js/runtime-main.6a82c300.js"
+  },
+  {
+    "revision": "e225856b39237f8c58f4c67d5023ac32",
+    "url": "/static/media/happy_otter_transparent.e225856b.png"
+  }
+]);
