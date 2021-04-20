622bbcd4c6159c6f5745de1cd9b4646c896abee8
AttyC <antoniajcronin@gmail.com>
2021-04-20 08:35:14 +0100

    remove unused comments

--- src/style/theme.js
+++ src/style/theme.js
@@ -1,13 +1,6 @@
 /*
   THEMING
-  - Store any colours in relevant objects below.
-  - Ensure that themes.haven and themes.opus have the exact same keys
-  - Only store themes.haven/themes.opus keys where there is a difference between environments
-  - Keep any shared styles within the components e.g. the horizontal header/footer border in the conversation area has the same styles for both brands so no need to store any related values in the themes object
-*/
-
-/*
-  Colour names in shared/haven/opusColours come from 'color description' section of www.colorhexa.com
+  Store any colours in relevant objects below.
 */
 
 export const colours = {
